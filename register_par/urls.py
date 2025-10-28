5a59e30ab74e930af1d16b6b33fa32fe8a2aa911
Canexy <mariocanalejas@hotmail.com>
2025-10-28 20:51:16 +0100

    Nuevo: Creación Vistas

--- register_par/urls.py
+++ register_par/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
 
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('equipos/', views.ListaEquiposView.as_view(), name='lista_equipos'),
 ]
--- register_par/views.py
+++ register_par/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
 
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Equipos
+
+class ListaEquiposView(ListView):
+    model = Equipos
+    template_name = 'register_par/equipos.html'
 
 def index(request):
     return HttpResponse("Hello, world. You're at the register index.")
