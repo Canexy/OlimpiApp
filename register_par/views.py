a3ce94ef6ea4445c9ef34a29915b1a102c4d9b7e
Canexy <mariocanalejas@hotmail.com>
2025-11-11 17:47:53 +0100

    Implementación no finalizada: forms.py

--- register_par/views.py
+++ register_par/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from .models import Equipos
 
+# # No implementado aún.
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import NameForm
+
+#
+
 class ListaEquiposView(ListView):
     model = Equipos
     template_name = 'register_par/lista_equipos.html'
@@ -13,3 +22,15 @@ class DetalleEquipoView(DetailView):
 
 def index(request):
     return HttpResponse("Hello, world. You're at the register index.")
+
+# No implementado aún.
+
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+        else:
+            form = NameForm()
+
+        return render(request, 'register_par/name.html', {'form': form})
